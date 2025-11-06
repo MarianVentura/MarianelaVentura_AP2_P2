9198cf5444c879c9e9e7deefe9ae3cdbc3e22f9e
MarianVentura <marianv1119@gmail.com>
2025-11-05 21:10:44 -0400

    Cree el caso de uso GetGastosUseCase.kt

---
+++ app/src/main/java/edu/ucne/marianelaventura_ap2_p2/domain/usecase/GetGastosUseCase.kt
@@ -0,0 +1,16 @@
+package edu.ucne.marianelaventura_ap2_p2.domain.usecase
+
+import edu.ucne.marianelaventura_ap2_p2.data.remote.Resource
+import edu.ucne.marianelaventura_ap2_p2.domain.model.Gasto
+import edu.ucne.marianelaventura_ap2_p2.domain.repository.GastoRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetGastosUseCase @Inject constructor(
+    private val repository: GastoRepository
+) {
+    operator fun invoke(): Flow<Resource<List<Gasto>>> {
+        return repository.getGastos()
+    }
+}
+
